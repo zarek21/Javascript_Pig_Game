5267ed0a9bfb7d6142cfa70b67c2fe4a35c2289a
Zarek Saleme <jorge.saleme77@gmail.com>
2025-09-06 11:58:48 -0600

    :sparkles finish logic to change player active

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 "use strict";
 
 // Selecting elements
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.getElementById("score--0");
 const score1El = document.getElementById("score--1");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
-
-const player0El = document.querySelector(".player--0");
-const player1El = document.querySelector(".player--1");
 
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
@@ -19,8 +18,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden"); // Hide dice
 
-// Score variable
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 // Rolling dice functionality
 btnRoll.addEventListener("click", function () {
@@ -34,13 +34,14 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore; // CHANGE LATER
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player and reset score to 0
-    player0El.classList.remove("player--active");
-    player1El.classList.add("player--active");
-    current0El.textContent = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
     currentScore = 0;
-    current1El.textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
